d113669fb1494a4449f9861a5b37f0582ff9b03e
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-06-22 12:28:12 +0900

    setting gemfille for rspec

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactroyBot.define do
+  factroy :user do
+    password = Faker::Internet.password(min_length: 8)
+    name {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password {password}
+    password_confirmation {password}
+  end
+end
