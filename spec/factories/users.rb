e0d804dbe9d6e27c49bfcc61aa423a091b1f0d76
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-06-22 12:38:17 +0900

    coding model test for message Model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
-FactroyBot.define do
-  factroy :user do
+FactoryBot.define do
+  factory :user do
     password = Faker::Internet.password(min_length: 8)
     name {Faker::Name.last_name}
     email {Faker::Internet.free_email}
