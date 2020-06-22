b96054261945216fffee7fd57ec2f1f8f0d8b224
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-06-22 11:57:54 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# README
+
+This README would normally document whatever steps are necessary to get the
+application up and running.
+
+Things you may want to cover:
+
+* Ruby version
+
+* System dependencies
+
+* Configuration
+
+* Database creation
+
+* Database initialization
+
+* How to run the test suite
+
+* Services (job queues, cache servers, search engines, etc.)
+
+* Deployment instructions
+
+## 中間提出用Chat-Space
+
+### 作成内容
+- チャット画面のhaml & scss
+- ログイン機能の実装
+  - サインアップ項目の追加
+- グループ作成機能
+  - 登録ユーザーの指定
+  - グループ編集機能
+- チャット送信機能の実装
+  - サイドバーにグループ一覧と最新のコメントを提示
+  - 画像投稿も可能
---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,30 @@
+class MessagesController < ApplicationController
+  before_action :set_up
+  def index
+      @message = Message.new
+      @messages = @group.messages.includes(:user)
+      @groups = current_user.groups
+  end
+
+  def create
+      @message = @group.messages.new(messages_params)
+      if @message.save
+        respond_to do |format|
+          format.json
+        end
+      else
+        @messages = @group.messages.includes(:user)
+        flash.now[:alert] = 'メッセージを入力してください'
+        render :index
+      end
+  end
+
+  private
+  def messages_params
+      params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_up
+    @group = Group.find(params[:group_id])
+  end
+end
